fd6be2b14cf491736cca3acf945b558e64594105
RealSWOO <swoo226@oton.kr>
2025-03-28 16:30:48 +0900

    Your commit message

---
+++ download_dataset.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+
+print(path_to_zip)
