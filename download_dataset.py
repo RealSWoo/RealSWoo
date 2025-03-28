fe7592b2991cc0734df773bed97684ce6b996b91
RealSWOO <swoo226@oton.kr>
2025-03-28 16:37:40 +0900

    Remove multiple files from GitHub repository

--- download_dataset.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
-
-path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
-
-print(path_to_zip)
